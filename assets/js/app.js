71ea7ee3e0c0477d258df67b39b164c944007e05
Shawn Assour <shawn.assour@sailpoint.com>
2017-03-03 10:47:29 -0600

    added styles and ratings

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,17 +18,13 @@ window.onload = init;
 function init() {
 	//initialize variables
 	topics = ['cat','dog','horse','fish','bird','snake','cow','shark','parrot'];
-
 	generateButtons(topics);
-
-	// $(document).on("click", ".giphy", displayGifs());
 }
 
 function animateGif() {
 	//when a gif is clicked make it animate
 	$('.gif-img').on('click', function() {
 		state = $(this).attr('state');
-		console.log(state);
 		if (state == 'still') {
 			$(this).attr('src', $(this).attr('animated-url'));
 			$(this).attr('state', 'animated');
@@ -42,7 +38,6 @@ function animateGif() {
 function generateButtons(topics) {
 	//generate inital buttons for display
 	$('#gif-buttons').empty();
-
 	for (var i = 0; i < topics.length; i++) {
 		//take topics array and create initial list of buttons
 		gifButton = $('<button>').html(topics[i]);
@@ -50,7 +45,6 @@ function generateButtons(topics) {
 		gifButton.attr('value', topics[i]);
 		$('#gif-buttons').append(gifButton);
 	}
-
 	$('.giphy').on('click', function(){
 		$('#gif-display').html('');
 		displayGifs(this.value);
@@ -69,27 +63,31 @@ function createNewButton() {
 function displayGifs(choice) {
 	//use AJAX call to grab gif images from API and display on the page
 	key = 'dc6zaTOxFJmzC';
-	queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + choice + '&limit=10&fmt=json' + '&api_key=' + key;
-
+	queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + choice + '&limit=10&fmt=json&rating=pg&rating=g&rating=y&api_key=' + key;
 	$.ajax({
 		url: queryURL,
 		query: 'GET'
 	}).done(function(response) {
 		console.log(response);
 		for (var i = 0; i < response.data.length; i++) {
-			// gifDiv = $('<div>');
-			// gifDiv.addClass('gif-response');
+			gifDiv = $('<div>');
+			gifDiv.addClass('gif-response');
+
+			var gifRating = $('<span>');
+			gifRating.attr('class', 'gif-rating');
+			gifRating.html('Rating: ' + response.data[i].rating);
 
 			gifImage = $('<img>');
 			gifImage.addClass('gif-img');
 			gifImage.attr('still-url', response.data[i].images.fixed_height_still.url); //grab still image url
 			gifImage.attr('animated-url', response.data[i].images.fixed_height.url); //grab animated image url
-			gifImage.attr('state', 'still');
+			gifImage.attr('state', 'still'); //set state attribute
 			gifImage.attr('src', response.data[i].images.fixed_height_still.url); //initial img will be still
 
-			// gifDiv.append(gifImage);
+			gifDiv.append(gifRating);
+			gifDiv.append(gifImage);
 
-			$('#gif-display').append(gifImage);
+			$('#gif-display').append(gifDiv);
 		}
 		animateGif();
 	});
